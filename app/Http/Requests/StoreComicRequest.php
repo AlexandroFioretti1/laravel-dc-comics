22f6e8cd4eb291c0fe3da23408b74b4ead97e5c8
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-06-01 12:02:31 +0200

    fix layout edit.blade.php + add request(update\store)

---
+++ app/Http/Requests/StoreComicRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:7|max:25',
+            'description' => 'required|min:1|max:5000',
+            'thumb' => 'required|min:10|max:290',
+            'price' => 'required|min:1|max:999',
+            'series' => 'required|min:1|max:2000',
+            'sale_date' => 'required|min:1|max:5000',
+        ];
+    }
+}
--- resources/views/admin/comic/show.blade.php
+++ resources/views/admin/comic/show.blade.php
@@ -5,11 +5,16 @@
 
 <div class="container">
     <div class="row">
-        <div class="col">
-            <div class="card ">
-                <img class="card-img-top text-center img-fluid" height="250" src="{{$comic->thumb}}" alt="#">
-                <h4>{{$comic->title}}</h4>
-                <p>{{$comic->description}}</p>
+        <div class="col ">
+            <div class="card text-center">
+                <div class="text-center m-3">
+                    <img class="" width="450" src="{{$comic->thumb}}" alt="#">
+                </div>
+                <h4>Titolo:{{$comic->title}}</h4>
+                <p>Descrizione:{{$comic->description}}</p>
+                <p>Thumb:{{$comic->thum}}</p>
+                <p>Price:{{$comic->price}}</p>
+                <p>Series:{{$comic->series}}</p>
             </div>
         </div>
     </div>
